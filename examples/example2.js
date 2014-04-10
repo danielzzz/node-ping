0645f9f3a41e409245f595fa1f8e601f3eabddf9
Mond Wan <mondwan.1015@gmail.com>
2014-04-10 23:49:07 +0800

    Fix conflict on Merge #8 and #9
    1. A new js file for separte the promise wrapper and the traditional callback
    wrapper
    2. Add markdown syntax on README and rename it to README.md
    3. Modify example2.js since the library structure changed
    4. Add a reference on index.js for calling promise wrapper
    5. Some coding changed in ping-sys due to JSHint warning
    
    XXX: I try my best to apply codes from
    https://github.com/danielzzz/node-ping/commit/4c3ea4d82e9a06d7d85aa66636c256b904ed448e
    to the trunk. Since I do not have a Node in Window platform, no
    guarantee for the correctness on window part.
    
    NOTE: I have tried on Ubuntu. It works well

--- examples/example2.js
+++ examples/example2.js
@@ -3,17 +3,20 @@ var ping = require("../index");
 var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
 
 hosts.forEach(function (host) {
-    ping.sys.promise_probe(host)
+    ping.promise.probe(host)
         .then(function (res) {
             console.log(res);
-        });
+        })
+        .done();
 });
 
 hosts.forEach(function (host) {
-    ping.sys.promise_probe(host, {
+    ping.promise.probe(host, {
         timeout: 10,
         extra: ["-i 2"]
-    }).then(function (res) {
+    })
+    .then(function (res) {
             console.log(res);
-        });
+    })
+    .done();
 });
