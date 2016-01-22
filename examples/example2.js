d13601a4a0c86a080239592dabcad72a5746baab
Mond Wan <mondwan.1015@gmail.com>
2016-01-23 00:41:46 +0800

    Update documentation

--- examples/example2.js
+++ examples/example2.js
@@ -16,10 +16,24 @@ hosts.forEach(function (host) {
     // WARNING: -i 2 argument may not work in other platform like window
     ping.promise.probe(host, {
         timeout: 10,
-        extra: ["-i", "2"]
+        extra: ["-i 2"],
     })
     .then(function (res) {
-            console.log(res);
+        console.log(res);
+    })
+    .done();
+});
+
+// Running ping with some default argument gone
+hosts.forEach(function (host) {
+    // WARNING: -i 2 argument may not work in other platform like window
+    ping.promise.probe(host, {
+        timeout: false,
+        // Below extra arguments may not work in platforms other than linux
+        extra: ["-i 2"],
+    })
+    .then(function (res) {
+        console.log(res);
     })
     .done();
 });
