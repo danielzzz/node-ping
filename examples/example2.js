99fe4a397e0d63be6c7a3bdaedc14ca42572150e
Mondwan <victor2wen@gmail.com>
2017-02-20 22:21:52 +0800

    Filter out the first empty line
    refs #59

--- examples/example2.js
+++ examples/example2.js
@@ -16,7 +16,7 @@ hosts.forEach(function (host) {
     // WARNING: -i 2 argument may not work in other platform like window
     ping.promise.probe(host, {
         timeout: 10,
-        extra: ["-i 2"],
+        extra: ["-i", "2"],
     })
     .then(function (res) {
         console.log(res);
@@ -30,7 +30,7 @@ hosts.forEach(function (host) {
     ping.promise.probe(host, {
         timeout: false,
         // Below extra arguments may not work in platforms other than linux
-        extra: ["-i 2"],
+        extra: ["-i", "2"],
     })
     .then(function (res) {
         console.log(res);
