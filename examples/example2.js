6cb848837f893b3b0af2f0f7f831c56b733ed3de
woicha <35530478+woicha@users.noreply.github.com>
2018-02-08 20:54:27 +0100

    Some small 'eslint' issues

--- examples/example2.js
+++ examples/example2.js
@@ -1,4 +1,6 @@
-var ping = require("../index");
+'use strict';
+
+var ping = require('../index');
 
 var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
 
@@ -16,7 +18,7 @@ hosts.forEach(function (host) {
     // WARNING: -i 2 argument may not work in other platform like window
     ping.promise.probe(host, {
         timeout: 10,
-        extra: ["-i", "2"],
+        extra: ['-i', '2'],
     })
     .then(function (res) {
         console.log(res);
@@ -30,7 +32,7 @@ hosts.forEach(function (host) {
     ping.promise.probe(host, {
         timeout: false,
         // Below extra arguments may not work in platforms other than linux
-        extra: ["-i", "2"],
+        extra: ['-i', '2'],
     })
     .then(function (res) {
         console.log(res);
