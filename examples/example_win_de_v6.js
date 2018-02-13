ba7cabd0834feeadaba76c7bfbd5f13ee9a10b51
woicha <35530478+woicha@users.noreply.github.com>
2018-02-13 16:26:43 +0100

    Adding changed test examples after tests
    Successfully tested on my German Windows 10 system.

--- examples/example_win_de_v6.js
+++ examples/example_win_de_v6.js
@@ -4,17 +4,32 @@
 
 var ping = require('../index');
 
-var hosts = ['google.de']; // ['192.168.1.1', 'google.com', 'yahoo.com'];
+var hosts = ['google.de']; // , '192.168.1.1', 'google.com', 'yahoo.com'];
 
 // Running with custom config
 hosts.forEach(function (host) {
     ping.promise.probe(host, {
-        v6: true,
-        min_reply: 4,
-        sourceAddr: '3001:4cb0:0:f282:ddf1:bec9:1e0:bfa9',
+        // v6: true,
+        min_reply: 2,
+        sourceAddr: 'your NIC\'s IPv6 address',
+        // sourceAddr: false,
     })
     .then(function (res) {
+        console.log('\n');
         console.log(res);
     })
     .done();
+
+    // Running ping with some default argument gone
+    ping.sys.probe(host, function (isAlive) {
+        var msg = isAlive ?
+            'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+        console.log('\n');
+        console.log(msg);
+    }, {
+        timeout: false,
+        // v6: true,
+        min_reply: 2,
+        sourceAddr: 'your NIC\'s IPv6 address',
+    });
 });
