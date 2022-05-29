8d1b90ef65268dcbe5e173e94942a757e93d7487
Mond Wan <mondwan.1015@gmail.com>
2022-05-30 00:01:14 +0100

    Support showing coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,31 @@ module.exports = function (grunt) {
                 reporter: 'dot',
             },
         },
+        nyc_mocha: {
+            src: ['test/test-*.js'],
+            options: {
+                nyc: {
+                    coverage: {
+                        dir: 'dist/coverage',
+                        reporter: ['text', 'html', 'lcov'],
+                    },
+                },
+                mocha: {
+                    color: true,
+                    opts: ['--reporter', 'dot'],
+                },
+            },
+        },
+        coveralls: {
+            src: 'dist/coverage/*.info',
+            options: {},
+        },
     });
 
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-nyc-mocha');
+    grunt.loadNpmTasks('grunt-coveralls');
     grunt.registerTask('test', ['eslint', 'mochaTest']);
+    grunt.registerTask('coverage', ['nyc_mocha']);
 };
